4c35e3c83f9531eaa14d8da46afc74daf61ef379
sheikh-niloy <sheikh.niloy.info@gmail.com>
2024-03-02 03:18:03 +0600

    Commit

---
+++ Start.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+public class Start 
+{
+    public static void main(String[] args) 
+    {
+        for (int i=1; i<=5; i++)
+        {
+            for (int s=1; s<=5-i; s++)
+            {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i*2-1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
